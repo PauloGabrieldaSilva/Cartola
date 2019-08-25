6e9c490f387b3dd77708332a69172283a0fc070c
William <williamradi@gmail.com>
2019-08-25 18:34:38 -0300

    CHAMADA FUNCIONANDO 100%

--- index.php
+++ index.php
@@ -10,49 +10,31 @@ use Automattic\WooCommerce\Client;
 $cli = new Client("http://www.lojadocartolafc.com.br", "ck_073dd3b2e4d1dfb0ce78d721f3d133b3edcc77da", "cs_aa43eb6378e53ff458ee76d01e6cd988d9c05a9d");
 
 
-$todayOrder = [
-    'produto' => [
-        'nome'              => 'Camisa Goleiro',
-        'personalizacao'    => [
-            'tam'   => ['1', '2', '3'],
-            'nome'  => ['joao', 'jose', 'agnaldo'],
-            'num'   => ['12', '45', '30']
-        ],
-        'imagem'            => 'http://[bla].jpg',
-        'quantidade'        => '3',
-        'valor'             => '39'
-    ]
-];
-
-$picaOrder = [
-    'billing'       => [
-        'first_name' => 'Teste_01'
-    ],
-    'line_items'    => [
+$data = [
+    'line_items' => [
         [
-            'name'      => 'Camisa Goleiro',
-            'quantity'  => 3,
-            'price'     => '39,00',
+            'product_id' => 327,
+            'quantity' => 5,
             'meta_data' => [
-                'tam'   => ['1', '2', '3'],
-                'nome'  => ['joao', 'jose', 'agnaldo'],
-                'num'   => ['12', '45', '30'],
-                'img'   => 'www.google.com'
+                [
+                    'key' => 'nome_estampado',
+                    'value' => 'Paulo'
+                ],
+                [
+                    'key' => 'numero_estampado',
+                    'value' => '24'
+                ],
+                [
+                    'key' => 'tamanho',
+                    'value' => 'G'
+                ],
+                [
+                    'key' => 'url_da_imagem',
+                    'value' => 'www.sitebalbalbal.com.br'
+                ]
             ]
         ]
     ]
 ];
 
-$data = [
-    'line_items' => [
-        [
-            'name'  => 'Camisa - editada',
-            'product_id' => 74,
-            'quantity' => 3,
-            'tam'   => 1
-        ]
-    ]
-];
-
-$nota = ['note' => 'NOTA NOTA NOTA NOTA NOTA NOTA NOTA NOTA NOTA NOTA NOTA NOTA NOTA NOTA NOTA'];
-$cli->post('orders/321/notes', $nota);
+$cli->post('orders', $data);
